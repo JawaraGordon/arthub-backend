bd8518f35ba49f9fbc380c119f2002165f221d3c
JawaraGordon <jawaragordon@icloud.com>
2022-07-07 18:50:09 -0400

    cleaned up code

--- app/application_controller.rb
+++ app/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
-  # get '/accounts' do
-  #   accounts = Account.all
-  #   accounts.to_json
-  # end
-
 end
 
 
